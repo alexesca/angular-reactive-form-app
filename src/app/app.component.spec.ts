ddb314df7db2a63a62bed7a30f013edb39707fc5
alexesca <alexescamore@gmail.com>
2020-12-16 14:25:37 -0700

    Added form control model and view Tested view-to-model data flow Tested model-to-view data flow

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,36 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'reactive-forms'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('reactive-forms');
-  });
+  it('should update the value of the input field', () => {
+    const input = fixture.nativeElement.querySelector('input');
+    const event = createNewEvent('input');
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('reactive-forms app is running!');
+    input.value = 'red';
+    input.dispatchEvent(event);
+
+    expect(fixture.componentInstance.favoriteColorControl.value).toEqual('Red');
+  })
+
+  it('should update the value in the control', () => {
+    component.favoriteColorControl.setValue('Blue');
+
+    const input = fixture.nativeElement.querySelector('input');
+
+    expect(input.value).toBe('Blue');
   });
 });
