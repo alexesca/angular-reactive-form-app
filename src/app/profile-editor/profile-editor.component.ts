c6f4de1e669138227feffdacc4b0eb40ef97cc60
alexesca <alexescamore@gmail.com>
2020-12-16 14:58:56 -0700

    Added nested group to form

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -11,12 +11,31 @@ export class ProfileEditorComponent implements OnInit {
 
   profileForm = new FormGroup({
     firstName: new FormControl(''),
-    lastName: new FormControl('')
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   })
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.warn(this.profileForm.value)
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '491 W 19th st'
+      }
+    })
   }
 
 }
